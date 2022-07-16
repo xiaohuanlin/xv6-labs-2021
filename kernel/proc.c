ce12d69604d480eae9a0b870eb5b0b0afcb14646
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-07-16 14:55:09 -0400

    finish lab2 trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -292,6 +292,9 @@ fork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // copy trace mask
+  np->trace = p->trace;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
