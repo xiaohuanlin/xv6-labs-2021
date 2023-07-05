4278a36ae5b62aa5915e83c4e87b3c9611ba033a
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-07-05 07:35:37 -0400

    finish lab networking

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -20,6 +20,8 @@ static struct mbuf *rx_mbufs[RX_RING_SIZE];
 static volatile uint32 *regs;
 
 struct spinlock e1000_lock;
+struct spinlock e1000_tx_lock;
+struct spinlock e1000_rx_lock;
 
 // called by pci_init().
 // xregs is the memory address at which the
@@ -30,6 +32,8 @@ e1000_init(uint32 *xregs)
   int i;
 
   initlock(&e1000_lock, "e1000");
+  initlock(&e1000_rx_lock, "e1000_rx");
+  initlock(&e1000_tx_lock, "e1000_tx");
 
   regs = xregs;
 
@@ -102,7 +106,28 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+  acquire(&e1000_tx_lock);
+  int tail = regs[E1000_TDT];
+
+  if (!(tx_ring[tail].status & E1000_TXD_STAT_DD)) {
+    release(&e1000_tx_lock);
+    return 1;
+  }
+
+  struct mbuf *tmp, *cur = tx_mbufs[tail];
+  while (cur) {
+    tmp = cur;
+    cur = cur->next;
+    mbuffree(tmp);
+  }
+  tx_mbufs[tail] = m;
+
+  tx_ring[tail].addr = (uint64) m->head;
+  tx_ring[tail].length = m->len;
+  tx_ring[tail].cmd = E1000_TXD_CMD_EOP | E1000_TXD_CMD_RS;
+  regs[E1000_TDT] = (tail + 1) % TX_RING_SIZE;
+
+  release(&e1000_tx_lock);
   return 0;
 }
 
@@ -115,6 +140,31 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  acquire(&e1000_rx_lock);
+  while (1) {
+    int tail = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+
+    if (!(rx_ring[tail].status & E1000_RXD_STAT_DD)) {
+      release(&e1000_rx_lock);
+      return;
+    }
+
+    struct mbuf *m = rx_mbufs[tail];
+    m->head = (char *) rx_ring[tail].addr;
+    m->len = rx_ring[tail].length;
+    net_rx(m);
+
+    rx_mbufs[tail] = mbufalloc(0);
+    if (!rx_mbufs[tail]) {
+      panic("e1000");
+    }
+
+    rx_ring[tail].addr = (uint64) rx_mbufs[tail]->head;
+    rx_ring[tail].length = rx_mbufs[tail]->len;
+    rx_ring[tail].status = 0;
+    regs[E1000_RDT] = tail % RX_RING_SIZE;
+  }
+  release(&e1000_rx_lock);
 }
 
 void
